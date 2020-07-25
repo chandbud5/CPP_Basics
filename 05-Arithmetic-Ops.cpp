db01a76d527c179370015e39c062d3cd6183c985
Chand Bud <chandbud5@gmail.com>
2020-07-25 11:27:34 +0530

    Add files via upload

---
+++ 05-Arithmetic-Ops.cpp
@@ -0,0 +1,19 @@
+// Arithmetic Operators
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10,c = 3;
+    float t,b = 3, d = 10;
+    cout << a+c << endl;
+    cout << a-c << endl;
+    cout << a*c << endl;
+    t = a/c;
+    cout << t << endl;
+    t = d/b;
+    cout << t << endl;
+    cout << a%c << endl;
+}
+
